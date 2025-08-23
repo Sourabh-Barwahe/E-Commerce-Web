e3e29eb13960dd31cfef6090380d6c18588b9ca6
Sourabh-Barwahe <sourabhbarwahe@gmail.com>
2025-08-23 16:24:16 +0530

    Update Shop.jsx

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -11,10 +11,10 @@ const Shop = () => {
   const [newcollection, setNewCollection] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/popularinwomen') 
+    fetch('https://e-commerce-backend-1qh2.onrender.com/popularinwomen') 
             .then((res) => res.json()) 
             .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
+    fetch('https://e-commerce-backend-1qh2.onrender.com/newcollections') 
             .then((res) => res.json()) 
             .then((data) => setNewCollection(data))
     }
